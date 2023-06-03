cd57075262f392dcec58662b84a7b8dfca6ab918
unknown <joaomcarddoso@gmail.com>
2023-06-02 21:23:24 -0300

    voltando para estado da primeira tentativa

--- app.js
+++ app.js
@@ -1,5 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos") 
 console.log("aplicação está se conectando ao banco de dados")
-//mais um console log de teste
-console.log("teste pra ver se essa porra vai funcionar mesmo ou se eu só dei sorte 1 vez")
-console.log("enviando para o github")
